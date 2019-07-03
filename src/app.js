aaa3f2af3467cbb7b927a5be0c58b138f2ccbf4e
sudhir512kj <sureshmeena512@gmail.com>
2019-07-03 20:26:05 +0530

    toggle completion

--- src/app.js
+++ src/app.js
@@ -99,8 +99,8 @@ App = {
             $newTaskTemplate.find('.content').html(taskContent);
             $newTaskTemplate.find('input')
                 .prop('name', taskId)
-                .prop('checked', taskCompleted);
-            // .on('click', App.toggleCompleted)
+                .prop('checked', taskCompleted)
+                .on('click', App.toggleCompleted);
 
             // Put the task in the correct list
             if (taskCompleted) {
@@ -118,6 +118,13 @@ App = {
         App.setLoading(true);
         const content = $('#newTask').val();
         await App.todoList.createTask(content);
+        window.location.reload();
+    },
+
+    toggleCompleted: async (e) => {
+        App.setLoading(true);
+        const taskId = e.target.name;
+        await App.todoList.toggleCompleted(taskId);
         window.location.reload();
     },
 
